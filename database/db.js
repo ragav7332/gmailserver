d25b7efdb56c6b172665b513781aabe124cd30fc
ragav7332 <ragav7332@gmail.com>
2023-11-22 06:58:31 +0530

    11th commit

--- database/db.js
+++ database/db.js
@@ -4,7 +4,12 @@ import dotenv from "dotenv"
 dotenv.config()
 
 mongoose.set('strictQuery', true);
-let Connection = mongoose.connect(
-    process.env.MONGO_URI);
+const Connection = mongoose.connect(
+    process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
 
 export default Connection;
