b8162a61002688bf09310e520982476d3bee761a
ragav7332 <ragav7332@gmail.com>
2023-11-22 08:05:06 +0530

    14th commit

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -2,11 +2,11 @@ import express from "express";
 import { saveSentEmails, getEmails, saveDraftEmails, moveEmailsToBin, toggleStarredEmails, deleteEmails } from "../controller/mailController.js";
 const route = express.Router()
 
-route.post('/save', saveSentEmails)
+route.post('/emails/save', saveSentEmails)
 route.get('/emails/:type', getEmails)
-route.post('/save-draft', saveDraftEmails)
-route.post('/bin', moveEmailsToBin)
-route.post('/starred', toggleStarredEmails)
-route.delete('/delete', deleteEmails)
+route.post('emails/save-draft', saveDraftEmails)
+route.post('/emails/bin', moveEmailsToBin)
+route.post('/emails/starred', toggleStarredEmails)
+route.delete('/emails/delete', deleteEmails)
 
 export default route
